0b987b590825356763fcb03c08af5d441bbef8f0
panyuzhou666 <3285791820@qq.com>
2025-09-03 10:05:54 +0800

    ACAM

--- src/String/AC 自动机.cpp
+++ src/String/AC 自动机.cpp
@@ -1,24 +1,65 @@
-int ch[MAXN][26], fail[MAXN], q[MAXN], cnt = 0;
-int insert(const char *c) {
-	int x = 0;
-	while (*c) {
-		if (!ch[x][*c - 'a']) ch[x][*c - 'a'] = ++cnt;
-		x = ch[x][*c++ - 'a'];
-	}
-	return x; 
-}
-void getfail() { int x, head = 0, tail = 0;
-	for (int c = 0; c < 26; c++)
-		if (ch[0][c])
-			q[tail++] = ch[0][c];
-	while (head != tail) {
-		x = q[head++];
-		for (int c = 0; c < 26; c++) {
-			if (ch[x][c]) {
-				fail[ch[x][c]] = ch[fail[x]][c];
-				q[tail++] = ch[x][c];
-			}
-			else ch[x][c] = ch[fail[x]][c];
-		}
-	}
-}
+struct ACAM {
+    static constexpr int M = 26;
+    struct Node {
+        int len;
+        int link;
+        array<int, M> next;
+        Node() : len{0}, link{0}, next{} {}
+    };
+
+    vector<Node> t;
+
+    ACAM() { init(); }
+
+    void init() {
+        // 设置了一个0号节点长度是-1,避免build时特判
+        t.assign(2, Node());
+        t[0].next.fill(1);
+        t[0].len = -1;
+    }
+
+    int newNode() {
+        t.emplace_back();
+        return t.size() - 1;
+    }
+
+    int add(const std::string& a) {
+        int p = 1;
+        for (auto c : a) {
+            int x = c - 'a';
+            if (!t[p].next[x]) {
+                t[p].next[x] = newNode();
+                t[t[p].next[x]].len = t[p].len + 1;
+            }
+            p = t[p].next[x];
+        }
+        return p;
+    }
+
+    void build() {
+        queue<int> q;
+        q.push(1);
+
+        while (!q.empty()) {
+            int x = q.front();
+            q.pop();
+
+            for (int i = 0; i < M; i++) {
+                if (t[x].next[i] == 0) {
+                    t[x].next[i] = t[t[x].link].next[i];
+                } else {
+                    t[t[x].next[i]].link = t[t[x].link].next[i];
+                    q.push(t[x].next[i]);
+                }
+            }
+        }
+    }
+
+    int next(int p, int x) { return t[p].next[x]; }
+
+    int link(int p) { return t[p].link; }
+
+    int len(int p) { return t[p].len; }
+
+    int size() { return t.size(); }
+};
---
+++ src/String/acamoi.cpp
@@ -0,0 +1,39 @@
+int cnt = 0;
+#define K 26
+struct node {
+    int son[K], num, fail;
+    void clear() {
+        num = fail = 0;
+        memset(son, 0, sizeof(son));
+    }
+} tr[N];
+void insert(string s) {
+    int p = 0;
+    for (int i = 0; i < s.length(); i++) {
+        int ch = s[i] - 'a';
+        if (!tr[p].son[ch]) tr[p].son[ch] = ++cnt, tr[cnt].clear();
+        p = tr[p].son[ch];
+    }
+    tr[p].num++;
+}
+vector<int> e[N];
+void getfail() {
+    queue<int> q;
+    for (int i = 0; i < K; i++)
+        if (tr[0].son[i]) q.push(tr[0].son[i]), e[0].push_back(tr[0].son[i]);
+    while (q.size()) {
+        int p = q.front();
+        q.pop();
+        for (int i = 0; i < K; i++) {
+            if (tr[p].son[i]) {
+                int a = tr[p].son[i], b = tr[tr[p].fail].son[i];
+                tr[a].fail = b, q.push(a);
+                e[b].push_back(a);
+            } else
+                tr[p].son[i] = tr[tr[p].fail].son[i];
+        }
+    }
+}
+// 清空：cnt=0,tr[0].clear();
+// for(int i=1;i<=n;i++) insert(s[i]);
+// for(Int i=1;i<=cnt;i++) e[i].clear();
