150b8d23d2b1650208995f467bbae8c382aac0c3
panyuzhou666 <3285791820@qq.com>
2025-10-19 11:57:55 +0800

    三元环

--- src/TreeandGraph/三元环.cpp
+++ src/TreeandGraph/三元环.cpp
@@ -1,7 +1,25 @@
-int triple_loop(n,m,X[],Y[]){ //n为点数,m为边数,X[i]-Y[i]为连边 
-	fr(i,1,m)du[X[i]]++,du[Y[i]]++;
-	fr(i,1,m)if(du[X[i]]<du[Y[i]] || du[X[i]]==du[Y[i]] && X[i]<Y[i]) ins(X[i],Y[i]); else ins(Y[i],X[i]);
-	fr(x,1,n){
-		tour(i,x)v[la[i]]=x;
-		tour(i,x)tour(j,la[i])if(v[la[j]]==x)ans++;
-	}return ans;}
+// 无向图三四元环计数 无重边和自环
+static int id[sz], rnk[sz];
+rep(i, 1, n) id[i] = i;
+sort(id + 1, id + n + 1,
+     [](int x, int y) { return pii{deg[x], x} < pii{deg[y], y}; });
+rep(i, 1, n) rnk[id[i]] = i;
+rep(i, 1, n) for (auto v : V[i]) if (rnk[v] > rnk[i]) V2[i].push_back(v);
+int ans3 = 0;  // 3-cycle
+rep(i, 1, n) {
+    static int vis[sz];
+    for (auto v : V2[i]) vis[v] = 1;
+    for (auto v1 : V2[i])
+        for (auto v2 : V2[v1])
+            if (vis[v2]) ++ans3;  // (i,v1,v2)
+    for (auto v : V2[i]) vis[v] = 0;
+}
+ll ans4 = 0;  // 4-cycle
+rep(i, 1, n) {
+    static int vis[sz];
+    for (auto v1 : V[i])
+        for (auto v2 : V2[v1])
+            if (rnk[v2] > rnk[i]) ans4 += vis[v2], vis[v2]++;
+    for (auto v1 : V[i])
+        for (auto v2 : V2[v1]) vis[v2] = 0;
+}
